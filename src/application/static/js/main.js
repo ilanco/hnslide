8ec229418af1c6b8daf8d640fc54e689676dcaa5
Ilan Cohen <ilanco@gmail.com>
2012-12-04 00:25:26 +0000

    added: load original article in frame

--- src/application/static/js/main.js
+++ src/application/static/js/main.js
@@ -17,12 +17,54 @@ var Utils = {
 
 /* Your custom JavaScript here */
 
+function loadExternal(url, callback) {
+    var container = $('#external .frame');
+    container.empty();
+
+    var frame = $('<iframe />', {
+        name: 'frame1',
+        id: 'frame1',
+        src: url
+    });
+    container.append(frame);
+
+    frame.onload = callback;
+}
+
 $(document).ready(function() {
     $('#slides').superslides({
         play: false,
         slide_speed: 'fast',
         pagination: true,
         hashchange: true
+    });
+
+    $('.entries li').on('click', 'a.link.title', function(event) {
+        event.preventDefault();
+
+        $('#external').show();
+        $('.slides-pagination, .slides-navigation').hide();
+        $('#slides').animate({
+            height: '100px',
+            opacity: 0.25,
+        }, 800, function() {
+        });
+
+        loadExternal(this.href, function() {
+            console.log('loaded');
+        });
+    });
+
+    $('#external').on('click', 'a.close', function(event) {
+        $('#slides').show();
+        $('#slides').animate({
+            height: '100%',
+            opacity: 1
+        }, 800, function() {
+            $('#external').hide();
+        });
+
+        $('.slides-pagination, .slides-navigation').show();
     });
 });
 
